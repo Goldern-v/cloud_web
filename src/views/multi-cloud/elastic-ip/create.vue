51fbbc83a1edab44491d2679847cbc3883508fb4
longwl <longwenlin@idealsc.cn>
2024-05-30 17:45:09 +0800

    fix: 修改代码

--- src/views/multi-cloud/elastic-ip/create.vue
+++ src/views/multi-cloud/elastic-ip/create.vue
@@ -8,7 +8,7 @@
 import generalCreate from './components/create.vue'
 import { useResourcePool } from '@/utils/common/resource'
 
-const { isPublic, isPrivateVmware, isPrivateHuawei } = useResourcePool()
+const { isPublic, isPrivateHuawei } = useResourcePool()
 </script>
 
 <style scoped lang="scss"></style>
