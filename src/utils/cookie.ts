347ddd6554567dd06daf6d9ad2ec20296173142a
longwl <longwenlin@idealsc.cn>
2024-06-14 14:19:21 +0800

    feat: 替换js-cookies为vue3 cookies

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,16 +1,16 @@
-import Cookies from 'js-cookie'
-
+import { useCookies } from 'vue3-cookies'
+const { cookies } = useCookies()
 const TokenKey = 'Authorization'
 class cookie {
   getToken() {
-    return Cookies.get(TokenKey) //获取键名为Authorization的值
+    return cookies.get(TokenKey) //获取键名为Authorization的值
   }
   setToken(token: string) {
-    return Cookies.set(TokenKey, token) //设置键名Authorization，值为token
+    return cookies.set(TokenKey, token) //设置键名Authorization，值为token
   }
 
   removeToken() {
-    return Cookies.remove(TokenKey) //移除键名为Authorization的值
+    return cookies.remove(TokenKey) //移除键名为Authorization的值
   }
 }
 
