f3bca50b9f068faedbfd4d395324d85216441412
youjiazhi <youjiazhi@idealsc.cn>
2024-06-27 16:18:15 +0800

    fix: 修改代码

--- src/views/multi-cloud/certificate-manage/create.vue
+++ src/views/multi-cloud/certificate-manage/create.vue
@@ -15,6 +15,7 @@
       </el-form-item>
 
       <template v-if="form.type === 'server'">
+        
         <el-form-item label="证书来源">
           <div class="flex-column">
             <el-radio-group v-model="form.source">
